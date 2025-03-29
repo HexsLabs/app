00fb478049647f9e0bf682fb2cf181f01a6fcb76
arnavmehta7 <arnavmehta.cs@gmail.com>
2025-03-29 23:18:40 +0530

    removed dead logic

--- src/lib/services/apiService.ts
+++ src/lib/services/apiService.ts
@@ -58,7 +58,6 @@ class ApiService {
       method: 'POST',
       body: JSON.stringify({
         ...data,
-        // provider: process.env.NEXT_PUBLIC_PROVIDER_TO_USE as ProviderType || 'auto'
       }),
     });
   }
